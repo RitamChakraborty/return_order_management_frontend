daa2b7f70d484014550f5eb2ae6e15ddb8d97982
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-05-01 10:00:58 +0530

    Add angular material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './view/home/home.component';
 import {HttpClientModule} from "@angular/common/http";
 import {LoginComponent} from './view/login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {LoginComponent} from './view/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
